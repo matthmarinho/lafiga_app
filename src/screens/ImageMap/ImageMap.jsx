a2f4287fbf14191ac57e113d6026f93226d79f07
Matthaeus <matthaeusbm@gmail.com>
2021-07-25 17:28:15 -0300

    first commit

---
+++ src/screens/ImageMap/ImageMap.jsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react'
+import { ImageOverlay, MapContainer, Marker, Popup, Tooltip } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+import melee from '../../_assets/img/melee.jpg'
+import ostrov from '../../_assets/img/ostrov.jpg'
+import thosgrar from '../../_assets/img/thosgrar.jpg'
+import marker from '../../_assets/img/marker_city.png'
+
+var Leaflet = require('leaflet')
+
+export default function ImageMap(props) {
+    const [bounds, setBounds] = useState([[0, 0], [0, 0]])
+    const [center, setCenter] = useState([0, 0])
+    const [loaded, setLoaded] = useState(false)
+    const [image, setImage] = useState()
+    const position = [1498, 1189]
+    const thisIcon = new Leaflet.Icon({
+        iconUrl: marker,
+        iconAnchor: new Leaflet.Point(16, 16),
+        iconSize: [25, 25],
+    })
+
+    useEffect(() => {
+        setLoaded(false);
+        // let imageName = require('../../_assets/img/' + props.mapValue + '.jpg')
+        let img = new Image();
+        // img.src = imageName.default
+        img.src = melee
+        img.onload = async () => {
+            setImage(img)
+            setCenter([img.height/2, img.width/2])
+            setBounds([[0, 0], [img.height, img.width]])
+            setLoaded(true)
+        }
+    }, [props.mapValue])
+
+    return (
+        true && (
+            <MapContainer
+                center={center}
+                bounds={bounds}
+                maxBounds={bounds}
+                maxBoundsViscosity={1}
+                crs={Leaflet.CRS.Simple}
+                maxZoom={1}
+                minZoom={-2}
+                style={{ height: '100%', width: '100%', background: 'black', display: 'flex' }}
+            >
+                <ImageOverlay
+                    // url={image.src}
+                    url={melee}
+                    bounds={bounds}
+                />
+                <Marker icon={thisIcon} position={position}>
+                    <Popup>
+                        O Reino de Aberama Gold
+                    </Popup>
+                    <Tooltip>O Reino de Aberama Gold</Tooltip>
+                </Marker>
+            </MapContainer>
+        )
+    )
+}
---
+++ src/screens/NavBar/NavBar.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import Body from '../Body/Body'
+import ImageMap from '../ImageMap/ImageMap'
+import logo from '../../_assets/img/logo.png'
+
+export default function NavBar() {
+    const [showSidebar, setShowSidebar] = useState(false);
+    return (
+        <React.Fragment>
+            <aside class="relative bg-sidebar h-screen w-64 hidden sm:block shadow-xl">
+                <div class="p-6">
+                    <a class="text-white text-3xl font-semibold uppercase hover:text-gray-300">Saite</a>
+                    {/* <button class="w-full bg-white cta-btn font-semibold py-2 mt-5 rounded-br-lg rounded-bl-lg rounded-tr-lg shadow-lg hover:shadow-xl hover:bg-gray-300 flex items-center justify-center">
+            <i class="fas fa-plus mr-3"></i> New Report
+          </button> */}
+                </div>
+                <nav class="text-white text-base font-semibold pt-3">
+                    <a onClick={() => {console.log('preda')}} class="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item">
+                        <i class="fas fa-tachometer-alt mr-3"></i>
+                        Continentes
+                    </a>
+                </nav>
+            </aside>
+
+            {/* <div class="w-full flex flex-col h-screen overflow-y-hidden">
+                <header class="w-full items-center bg-white py-2 px-6 hidden sm:flex">
+                    <div class="w-1/2"></div>
+                </header>
+            </div> */}
+
+            {/* <Body showSidebar={showSidebar} /> */}
+        </React.Fragment>
+    )
+}
