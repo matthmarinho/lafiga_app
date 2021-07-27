563473302d261182f39c45bc2cc3f84a67fdc6cf
alysson <alysson.soares@hotmail.com.br>
2021-07-26 21:41:09 -0300

    fix auth

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,11 +1,10 @@
 import { sendToken, sendUserInfo } from "../screens/Login/LoginModal";
-export const getToken = sendToken;
+// export const getToken = sendToken;
 export const TOKEN_KEY = "@lafiga-token";
 export const USER_INFO = "@user-info";
 export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null;
 export const userData = () => JSON.parse(localStorage.getItem(USER_INFO));
-console.log(userData);
-// export const getToken = () => localStorage.getItem(TOKEN_KEY);
+export const getToken = () => localStorage.getItem(TOKEN_KEY);
 export const login = (auth) => {
   console.log(auth);
   const { auth_token, user_infos } = auth;
